68b1739272e892100da8824b78fe686ec95a6cc1
Antonio Sucre <antonio.sucre@indikatore.com>
2021-09-26 21:44:47 -0300

    adding-jumbotron

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './calculator.css';
+import { Jumbotron } from 'react-bootstrap';
 
 function Calculator() {
   return (
-    <h1>Calculator App</h1>
+    <Jumbotron style={{
+      background: 'transparent !important',
+      backgroundColor: '#007bff',
+      padding: '5px',
+      margin: '5px',
+      width: '400px'
+    }}>
+
+    </Jumbotron>
   );
 }
 
