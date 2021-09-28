fcd094752468f88a18012f455a282fbf357b35ad
Antonio Sucre <antonio.sucre@indikatore.com>
2021-09-27 22:19:10 -0300

    using component with service

--- src/calculator.js
+++ src/calculator.js
@@ -3,17 +3,63 @@ import './calculator.css';
 import {
   Jumbotron, Container, Row, Col, Button, Form
 } from 'react-bootstrap';
+import CalculatorService from './calculator.service';
 
 function Calculator() {
 
+  const [calculate, concatNumber, SUM, SUB, MUL, DIV] = CalculatorService();
+
   const [txtNumbers, setTxtNumbers] = useState('0');
+  const [number1, setNumber1] = useState('0');
+  const [number2, setNumber2] = useState(null);
+  const [operation, setOperation] = useState(null);
+
 
   function addNumber(number) {
-    setTxtNumbers(txtNumbers + number);
+    let result;
+    if (operation === null) {
+      result = concatNumber(number1, number);
+      setNumber1(result);
+    } else {
+      result = concatNumber(number2, number);
+      setNumber2(result);
+    }
+
+    setTxtNumbers(result);
   }
 
   function defineOperation(op) {
-    setTxtNumbers(op);
+    // apenas define a operação caso ela não exista
+    if (operation === null) {
+      setOperation(op);
+      return;
+    }
+    // caso a operação estiver definida e numero2 selecionado, realizar o calculo da operação
+    if (number2 !== null) {
+      const result = calculate(parseFloat(number1), parseFloat(number2), operation);
+      setOperation(op);
+      setNumber1(result.toString());
+      setNumber2(null);
+      setTxtNumbers(result.toString());
+    }
+  }
+
+  function calculateAction() {
+    if (number2 === null) {
+      return;
+    }
+
+    const result = calculate(parseFloat(number1), parseFloat(number2), operation);
+    setNumber1(result.toString());
+    setNumber2(null);
+    setTxtNumbers(result.toString());
+  }
+
+  function clean() {
+    setTxtNumbers('0');
+    setNumber1('0');
+    setNumber2(null);
+    setOperation(null);
   }
 
   return (
@@ -27,7 +73,7 @@ function Calculator() {
       <Container>
         <Row>
           <Col xs="3">
-            <Button variant="danger">C</Button>
+            <Button variant="danger" onClick={clean} >C</Button>
           </Col>
           <Col xs="9">
             <Form.Control type="text"
@@ -88,10 +134,10 @@ function Calculator() {
             <Button variant="light" onClick={() => addNumber('0')}>0</Button>
           </Col>
           <Col>
-            <Button variant="light">.</Button>
+            <Button variant="light" onClick={() => addNumber('.')} >.</Button>
           </Col>
           <Col>
-            <Button variant="success">=</Button>
+            <Button variant="success" onClick={calculateAction}>=</Button>
           </Col>
           <Col>
             <Button variant="warning" onClick={() => defineOperation('+')}>+</Button>
